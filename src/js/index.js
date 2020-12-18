dcb8358f90a491801836e00f1372d682ade9ed28
Lukasz Kwiatkowski <Kwiatkovsky1999@wp.pl>
2020-12-18 16:59:49 +0100

    add local storage

--- src/js/index.js
+++ src/js/index.js
@@ -13,17 +13,28 @@ const buttonAdd = document.querySelector(`.button--add__js`)
 const buttonRemove = document.querySelector(`.button--remove__js`);
 const key = new Date().toLocaleString().slice(0, 10);
 
-console.log(key);
 
 let currentGlassCounter = 0;
 
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+    currentGlassCounter = localStorageValue;
+} else {
+    localStorage.setItem(key, 0);
+}
+
+
+
 buttonAdd.addEventListener(`click`, () =>{
-   currentGlassCounter++;
+   currentGlassCounter = currentGlassCounter +1;
    glassCounter.innerHTML = currentGlassCounter;
+   localStorage.setItem(key, currentGlassCounter);
 });
 buttonRemove.addEventListener(`click`, () =>{
     if(currentGlassCounter > 0) {
         currentGlassCounter--;
     }
     glassCounter.innerHTML = currentGlassCounter;
-});
+    localStorage.setItem(key, currentGlassCounter);
+})
