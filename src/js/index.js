4dc29cd7ed517578e6c62c7caf00db5a01144ef4
Lukasz Kwiatkowski <Kwiatkovsky1999@wp.pl>
2020-12-18 16:19:08 +0100

    add functions for buttons

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,22 @@ import '../scss/main.scss';
 
 console.log('HELLO 🚀')
 
-const glassCounter = document.querySelector(`glass--counter__js`);
-const buttonAdd = document.querySelector(`button--add__js`)
-const buttonRemove = document.querySelector(`button--remove__js`)
+const glassCounter = document.querySelector(`.glass--counter__js`);
+const buttonAdd = document.querySelector(`.button--add__js`)
+const buttonRemove = document.querySelector(`.button--remove__js`);
+const key = new Date().toLocaleString().slice(0, 10);
+
+console.log(key);
+
+let currentGlassCounter = 0;
+
+buttonAdd.addEventListener(`click`, () =>{
+   currentGlassCounter++;
+   glassCounter.innerHTML = currentGlassCounter;
+});
+buttonRemove.addEventListener(`click`, () =>{
+    if(currentGlassCounter > 0) {
+        currentGlassCounter--;
+    }
+    glassCounter.innerHTML = currentGlassCounter;
+});
